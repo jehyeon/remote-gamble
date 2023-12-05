05ff397b716764e71abc9cfc837ffb34d0498e53
jehyeon <hyeon.rot@gmail.com>
2023-12-05 17:48:48 +0900

    temp

---
+++ Source/RemoteGamble/GamerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GamerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REMOTEGAMBLE_API AGamerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
